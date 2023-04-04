1660d711e0ad699a9084ce0191c6d0899a33ed5e
TuftyRairu <craususrhyle@gmail.com>
2023-04-04 20:47:46 +0800

    Final Grading System

--- functions/main.js
+++ functions/main.js
@@ -1,29 +1,29 @@
 function buttonPress(){
     let average, sum;
-    let c, p, r, t, m, u, a;
+    let m, e, s, pe, f, p, j;
 
-    c = Number(document.getElementById('comprog').value);
-    p = Number(document.getElementById('purcom').value);
-    r = Number(document.getElementById('rph').value);
-    t = Number(document.getElementById('tcw').value);
-    m = Number(document.getElementById('mmw').value);
-    u = Number(document.getElementById('uts').value);
-    a = Number(document.getElementById('artapp').value);
+    m = Number(document.getElementById('math').value);
+    e = Number(document.getElementById('english').value);
+    s = Number(document.getElementById('math').value);
+    pe = Number(document.getElementById('science').value);
+    f = Number(document.getElementById('pe').value);
+    p = Number(document.getElementById('psychology').value);
+    j = Number(document.getElementById('japanese').value);
 
-    sum = c + p + r + t + m + u + a;
+    sum = m + e + s + pe + f + p + j;
     average =  sum / 7;
 
     document.getElementById('ave').innerHTML = 'Average - ' + parseFloat(average).toFixed(2);
 
-    if (document.getElementById('comprog').value == '' || document.getElementById('purcom').value == '' || document.getElementById('rph').value == '' || document.getElementById('tcw').value == '' || document.getElementById('mmw').value == '' || document.getElementById('uts').value == '' || document.getElementById('artapp').value == ''){
+    if (document.getElementById('math').value == '' || document.getElementById('english').value == '' || document.getElementById('science').value == '' || document.getElementById('pe').value == '' || document.getElementById('filipino').value == '' || document.getElementById('psychology').value == '' || document.getElementById('japanese').value == ''){
         window.alert('ERROR!\nEMPTY TEXT FIELD');
-        document.getElementById('comprog').value = 'ERROR!';
-        document.getElementById('purcom').value = 'ERROR!';
-        document.getElementById('rph').value = 'ERROR!';
-        document.getElementById('tcw').value = 'ERROR!';
-        document.getElementById('mmw').value = 'ERROR!';
-        document.getElementById('uts').value = 'ERROR!';
-        document.getElementById('artapp').value = 'ERROR!';
+        document.getElementById('math').value = 'ERROR!';
+        document.getElementById('english').value = 'ERROR!';
+        document.getElementById('science').value = 'ERROR!';
+        document.getElementById('pe').value = 'ERROR!';
+        document.getElementById('filipino').value = 'ERROR!';
+        document.getElementById('psychology').value = 'ERROR!';
+        document.getElementById('japanese').value = 'ERROR!';
         document.getElementById('ave').innerHTML = 'Average - ERROR!';
         document.getElementById('rem').innerHTML = 'Remark - ERROR!';
         document.getElementById('desc').innerHTML = 'Descriptor - ERROR!';
@@ -55,11 +55,11 @@ function reset(){
     document.getElementById('ave').innerHTML = 'Average - ';
     document.getElementById('rem').innerHTML = 'Remark - ';
     document.getElementById('desc').innerHTML = 'Descriptor - ';
-    document.getElementById('comprog').value = '';
-    document.getElementById('purcom').value = '';
-    document.getElementById('rph').value = '';
-    document.getElementById('tcw').value = '';
-    document.getElementById('mmw').value = '';
-    document.getElementById('uts').value = '';
-    document.getElementById('artapp').value = '';
+    document.getElementById('math').value = '';
+    document.getElementById('english').value = '';
+    document.getElementById('science').value = '';
+    document.getElementById('pe').value = '';
+    document.getElementById('filipino').value = '';
+    document.getElementById('psychology').value = '';
+    document.getElementById('japanese').value = '';
 }
